9eec400777d667b99a1fd5c73414f34f138c4923
Dale Wyttenbach <dwyttenbach@decare.com>
2016-11-16 14:05:51 -0600

    Simple upper case conversion

--- complete/src/main/java/hello/PersonItemProcessor.java
+++ complete/src/main/java/hello/PersonItemProcessor.java
@@ -1,29 +1,18 @@
 package hello;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.item.ItemProcessor;
 
 public class PersonItemProcessor implements ItemProcessor<Person, Person> {
 
   private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
   
-  private Map<String, String> map = new HashMap<String, String>();
-
-
   @Override
   public Person process(final Person person) throws Exception {
     final Person transformedPerson = new Person();
     for (String key : person.keySet()) {
-      String value = map.get(person.get(key));
-      if ( value == null ) {
-        value = "UNKNOWN";
-      }
-      transformedPerson.put(key, value);
+      transformedPerson.put(key, person.get(key).toUpperCase());
     }
 
     log.info("Converting (" + person + ") into (" + transformedPerson + ")");
