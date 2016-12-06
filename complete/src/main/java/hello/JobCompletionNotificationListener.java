87460d9f4d3f01291f9bf9a2795ae660f9a7b876
Dale Wyttenbach <dwyttenbach@decare.com>
2016-12-06 14:13:48 -0600

    Fix the listener which was throwing an error since we no longer create the PEOPLE table in the in-memory database

--- complete/src/main/java/hello/JobCompletionNotificationListener.java
+++ complete/src/main/java/hello/JobCompletionNotificationListener.java
@@ -1,18 +1,10 @@
 package hello;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,32 +12,12 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
 	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-	private final JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
 	@Override
 	public void afterJob(JobExecution jobExecution) {
 		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-			log.info("!!! JOB FINISHED! Time to verify the results");
-
-			List<Person> results = jdbcTemplate.query("SELECT first_name, last_name FROM people", new RowMapper<Person>() {
-				@Override
-				public Person mapRow(ResultSet rs, int row) throws SQLException {
-				  Person person = new Person();
-          person.put("firstName", rs.getString(1));
-          person.put("lastName", rs.getString(2));
-					return person;
-				}
-			});
-
-			for (Person person : results) {
-				log.info("Found <" + person + "> in the database.");
-			}
-
+      log.info("!!! JOB COMPLETED!");
+		} else {
+		  throw new IllegalStateException("Unexpected job execution status: " + jobExecution.getStatus());
 		}
 	}
 }
